f824d83cea19b2fc96cc012f8ce8d6212695fdbd
Aq7853gehc <aquib7583@gmail.com>
2025-01-01 20:20:59 +0530

    Remix Storage Factory complete (Inheritance , and other stuffs )

--- contracts/AddFiveStorage.sol
+++ contracts/AddFiveStorage.sol
@@ -3,9 +3,8 @@ pragma solidity ^0.8.24;
 
 import {SimpleStorage} from "contracts/SimpleStorage.sol";
 
-
 contract AddFiveStorage is SimpleStorage {
-    function sayHello() public  pure  returns(string memory){
-        return "hello";
+    function store(uint256 _newFavNumber) public override {
+        favNumber = _newFavNumber + 5;
     }
 }
--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,26 +2,27 @@
 pragma solidity ^0.8.24;
 
 contract SimpleStorage {
-    uint favNumber;
-    function store(uint _favNum) public {
+    uint256 favNumber;
+
+    function store(uint256 _favNum) public virtual {
         favNumber = _favNum;
     }
 
-    function retrive() public  view  returns(uint) {
-        return  favNumber;
+    function retrive() public view returns (uint256) {
+        return favNumber;
     }
 
     struct Person {
         string naem;
-        uint favnum;
+        uint256 favnum;
     }
 
-    Person[] public  listOfPeople;
+    Person[] public listOfPeople;
 
-    mapping (string=>uint) public  nameToFav;
+    mapping(string => uint256) public nameToFav;
 
-    function add_person(string memory _name , uint _favnum) public  {
-        listOfPeople.push(Person(_name,_favnum));
+    function add_person(string memory _name, uint256 _favnum) public {
+        listOfPeople.push(Person(_name, _favnum));
         nameToFav[_name] = _favnum;
     }
 }
